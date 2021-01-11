1d507d64bb4a6ea1bae64dc3c78a6f92b5f4979b
troospieler <troospieler1990@gmail.com>
2021-01-11 20:43:03 +0200

    Updates

---
+++ precache-manifest.63951481e30f66009118dcafbf3222e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1657d7d9b7604e12f852060e29f78a7d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23a0128c7e37fb45b4e",
+    "url": "./static/js/main.7413105d.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.63951481e30f66009118dcafbf3222e3.js"
 );
 
 self.addEventListener('message', (event) => {
